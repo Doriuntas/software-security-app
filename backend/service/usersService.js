0fe95f9bcc0e7aa5b53c22578f682bea17db8431
Doriuntas <97697797+Doriuntas@users.noreply.github.com>
2024-04-21 22:17:43 -0400

    Update usersService.js

--- backend/service/usersService.js
+++ backend/service/usersService.js
@@ -1,13 +1,49 @@
-/*
-Insert dependencies and functions here:
-    [import dependencies]
-    getAllUsers: Fetch all users from the database.
-    getUserById: Fetch a user by their ID from the database.
-    createUser: Create a new user with a hashed password.
-    updateUser: Update a user's information.
-    deleteUser: Delete a user from the database.
-    authenticateUser: Authenticate a user and generate a JWT token for authorized access.
-*/
+const bcrypt = require('bcryptjs');
+const userRepository = require('../repository/usersRepository');
+const User = require('../models/users');
+const jwt = require('jsonwebtoken');
+
+getAllUsers
+const getAllUsers = async () => {
+ return await usersRepository.findAll();
+};
+
+getUserById
+
+const getUserById = async (id) => {
+ return await usersRepository.findById(id);
+};
+
+createUser
+const createUser = async (user) => {
+ const hashedPassword = await bcrypt.hash(user.password, 10);
+ const newUser = new User(null, user.username, hashedPassword, user.email);
+ return await usersRepository.save(newUser);
+};
+
+updateUser
+
+const updateUser = async (id, user) => {
+ return await usersRepository.update(id, user);
+};
+
+deleteUser
+
+const deleteUser = async (id) => {
+ await usersRepository.remove(id);
+};
+
+authenticateUser
+const authenticateUser = async (username, password) => {
+ const user = await usersRepository.findByUsername(username);
+ if (user && await bcrypt.compare(password, user.password)) {
+ const token = jwt.sign({ id: user.id }, 'your_secret_key', { expiresIn: '1h' });
+ return { token };
+ }
+ return null;
+};
+
+
 
 
 module.exports = {
